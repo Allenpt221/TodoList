455a3f402ad66f6d7f31a034a48f1f81e9fd633c
allenpt221 <allenpt221@gmail.com>
2025-01-02 06:30:50 +0800

    Added updating the list and delete

--- backend/controller/TodoList.controller.js
+++ backend/controller/TodoList.controller.js
@@ -29,6 +29,10 @@ export const retrieveTask = async(req, res) => {
     try {
         const dataList = await TodoList.find({});
 
+        if(dataList.length === 0){
+            res.status(404).json({success: false, message: 'the database list is empty'});
+        }
+
         res.status(200).json({success:true, data: dataList});
     } catch (error) {
         console.log('server error in retrieveTask');
@@ -38,11 +42,40 @@ export const retrieveTask = async(req, res) => {
 
 export const updateTask = async (req, res) => {
     const { id } =  req.params;
-    if(!mongoose.Tp)
+    const Task = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        res.status(404).json({success: false, mesage: 'id is not found'});
+    }
+
     try {
-        
+        const updatedData = await TodoList.findByIdAndUpdate(id, Task, {new: true});
+        if(!updatedData){
+            res.status(404).json({success: false, message: 'The data cannot found'});
+        }
+
+        res.status(200).json({success: true, data: updatedData});
+
     } catch (error) {
         console.log('server error in updateTask');
+        res.status(500).json({success: false, message: 'Server is error'});
+    }
+};
+
+export const deleteTask = async (req, res) => {
+    const { id } = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        res.status(404).json({success: false, mesage: 'id is not found'});
+    }
+    try {
+        const deleteData = await TodoList.findByIdAndDelete(id);
+        if(!deleteData){
+            res.status(404).json({success: false, message: 'List already delete'});
+        }
+
+        res.status(200).json({success: true, message: 'sucessfully delete', data: deleteData});
+    } catch (error) {
+        console.log('server error in deleteTask');
         res.status(500).json({success: false, message: 'Server is error'});
     }
 };
--- backend/router/TodoList.router.js
+++ backend/router/TodoList.router.js
@@ -1,12 +1,13 @@
 import express from 'express';
-import { createTask, retrieveTask } from '../controller/TodoList.controller.js';
+import { createTask, deleteTask, retrieveTask, updateTask } from '../controller/TodoList.controller.js';
 
 
 const router = express.Router();
 
 router.post('/', createTask);
 router.get('/', retrieveTask);
-router.put('/:id', retrieveTask);
+router.put('/:id', updateTask);
+router.delete('/:id', deleteTask);
 
 
 export default router;
